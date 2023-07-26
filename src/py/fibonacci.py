6f3b5b23e7e118ebc3ba83aee1235d1cc81ca83f
Murilo Cunha <murilo@dataroots.io>
2023-07-26 12:30:34 +0200

    make it more verbose - clearer

--- src/py/fibonacci.py
+++ src/py/fibonacci.py
@@ -3,4 +3,6 @@
 
 def fib(n: int) -> int:
     """Calculate the nth Fibonacci number."""
-    return n if n <= 1 else fib(n - 2) + fib(n - 1)
+    if n <= 1:
+        return n
+    return fib(n - 2) + fib(n - 1)
