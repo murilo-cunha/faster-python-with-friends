e238efc074c5a2c77c615cb5479ec2254c481425
Murilo Cunha <murilo@dataroots.io>
2023-07-29 22:08:56 +0200

    add rust module for fib and triangles

--- src/rs/src/lib.rs
+++ src/rs/src/lib.rs
@@ -1,93 +1,82 @@
-use std::{collections::HashMap, env, fs};
-
+use pyo3::wrap_pymodule;
 use pyo3::{exceptions::PyTypeError, prelude::*, PyErr};
+use std::collections::HashMap;
 
-type NeighborNodes = Vec<u16>;
-type Graph = HashMap<u16, NeighborNodes>;
-
-/// Load undirected graph from CSV file with ` ` delimiter.
-fn load_graph(file_path: Option<&str>) -> Graph {
-    let _expected_token = "missing token in line";
-    let mut edges: Vec<(u16, u16)> = fs::read_to_string(
-        file_path.unwrap_or(&env::var("FB_DATA").expect("data file path not found")),
-    )
-    .expect("could not read file")
-    .lines()
-    .map(|l| {
-        let mut tokens = l.split(' ');
-        (
-            tokens.next().expect(_expected_token).parse().unwrap(),
-            tokens.next().expect(_expected_token).parse().unwrap(),
-        )
-    })
-    .collect();
-    edges.extend(edges.clone().iter().map(|&l| (l.1, l.0)));
-
-    let mut graph: Graph = HashMap::new();
-    for (node, neighbor) in edges {
-        let mut _neighbors = graph.entry(node).or_default();
-        _neighbors.push(neighbor);
-    }
-    graph
-}
-
-///     Calculate number of triangles.
+/// Calculate triangles for network.
 ///
-///     0. Track nodes visited and number of triangles
-///     1. For each node:
-///         a. Track neighbors visited
-///         b. Get it's neighbors
-///         c. If the neighbor node has not been visited, check it's neighbors
-///          ("far neighbor")
-///         d. If the "far neighbor" has not been visited (either nodes or neighbors), check
-///          that the current node is in the "far neighbor's" neighbors and increment
-///          triangle count if present
-///         e. Add neighbor node to neighbors visited
-///     2. Add node to nodes visited
-fn calc_triangles(graph: Graph) -> PyResult<u32> {
-    let mut num_triangles = 0;
+/// I.e.: use graph from https://snap.stanford.edu/data/ego-Facebook.html.
+#[pymodule]
+fn triangles(_py: Python, module: &PyModule) -> PyResult<()> {
+    type NeighborNodes = Vec<u16>;
+    type Graph = HashMap<u16, NeighborNodes>;
 
-    let mut visited: NeighborNodes = Vec::new();
-    for node in graph.keys() {
-        let mut neighbors_visited: NeighborNodes = Vec::new();
-        for neighbor in graph
-            .get(node)
-            .ok_or(PyErr::new::<PyTypeError, _>("key error"))?
-        {
-            if !visited.contains(neighbor) {
-                for far_neighbor in graph
-                    .get(neighbor)
-                    .ok_or(PyErr::new::<PyTypeError, _>("key error"))?
-                {
-                    if (!neighbors_visited.contains(far_neighbor))
-                        && (!visited.contains(far_neighbor))
-                        && (graph
-                            .get(far_neighbor)
-                            .ok_or(PyErr::new::<PyTypeError, _>("key error"))?
-                            .contains(node))
+    /// Calculate number of triangles.
+    ///
+    /// 0. Track nodes visited and number of triangles
+    /// 1. For each node:
+    ///     a. Track neighbors visited
+    ///     b. Get it's neighbors
+    ///     c. If the neighbor node has not been visited, check it's neighbors
+    ///      ("far neighbor")
+    ///     d. If the "far neighbor" has not been visited (either nodes or neighbors), check
+    ///      that the current node is in the "far neighbor's" neighbors and increment
+    ///      triangle count if present
+    ///     e. Add neighbor node to neighbors visited
+    /// 2. Add node to nodes visited
+    #[pyfn(module)]
+    fn calc_triangles(graph: Graph) -> PyResult<u32> {
+        let mut num_triangles = 0;
+
+        let mut visited: NeighborNodes = Vec::new();
+        for node in graph.keys() {
+            let mut neighbors_visited: NeighborNodes = Vec::new();
+            for neighbor in graph
+                .get(node)
+                .ok_or(PyErr::new::<PyTypeError, _>("key error"))?
+            {
+                if !visited.contains(neighbor) {
+                    for far_neighbor in graph
+                        .get(neighbor)
+                        .ok_or(PyErr::new::<PyTypeError, _>("key error"))?
                     {
-                        num_triangles += 1;
+                        if (!neighbors_visited.contains(far_neighbor))
+                            && (!visited.contains(far_neighbor))
+                            && (graph
+                                .get(far_neighbor)
+                                .ok_or(PyErr::new::<PyTypeError, _>("key error"))?
+                                .contains(node))
+                        {
+                            num_triangles += 1;
+                        }
                     }
                 }
+                neighbors_visited.push(*neighbor);
             }
-            neighbors_visited.push(*neighbor);
+            visited.push(*node);
         }
-        visited.push(*node);
+        Ok(num_triangles)
     }
-
-    Ok(num_triangles)
+    Ok(())
 }
 
-/// Loads graph from path `p` and calculate triangles.
-#[pyfunction]
-fn load_and_calc(p: Option<&str>) -> PyResult<u32> {
-    let graph = load_graph(p);
-    calc_triangles(graph)
-}
-
-/// Calculating triangles implemented in Rust.
+/// Fibonacci numbers.
 #[pymodule]
-fn rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(load_and_calc, m)?)?;
+fn fibonacci(_py: Python, module: &PyModule) -> PyResult<()> {
+    /// Calculate the nth Fibonacci number.
+    #[pyfn(module)]
+    fn fib(n: u32) -> PyResult<u32> {
+        if n <= 1 {
+            return Ok(n);
+        }
+        Ok(fib(n - 2)? + fib(n - 1)?)
+    }
+    Ok(())
+}
+
+/// Rust source code.
+#[pymodule]
+fn rs(_py: Python, module: &PyModule) -> PyResult<()> {
+    module.add_wrapped(wrap_pymodule!(triangles))?;
+    module.add_wrapped(wrap_pymodule!(fibonacci))?;
     Ok(())
 }
