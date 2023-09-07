d4f9dbcffcf35bfa3b305c82fbd968303ae0885b
Murilo Cunha <murilo@dataroots.io>
2023-09-07 16:26:56 +0100

    mv base url config - was misplaced

--- presentation/theme/vite.config.js
+++ presentation/theme/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import svgLoader from 'vite-svg-loader'
 
 export default defineConfig({
-  base: '/faster-python-with-friends',
   plugins: [svgLoader()],
 });
--- presentation/vite.config.js
+++ presentation/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import svgLoader from 'vite-svg-loader';
 
 export default defineConfig({
+  base: '/faster-python-with-friends',
   plugins: [svgLoader()],
 });
